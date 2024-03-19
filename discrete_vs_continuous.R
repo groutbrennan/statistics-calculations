146ac6291eb76d665187997b03b8c76fe7e73e11
Brennan Grout <groutb@orvis.com>
2024-03-19 12:46:11 -0400

    added discrete range table

--- discrete_vs_continuous.R
+++ discrete_vs_continuous.R
@@ -8,4 +8,28 @@
 # 3                               4                 0.100                   0.300
 # 4                               2                 0.050                   0.200
 # 5                               1                 0.025                   0.125
-# Total Customers               # 40              # MEAN                  # 1.68
+# Total Customers               # 40                               # MEAN = 1.68
+
+
+##### Standard Deviation
+#################                            
+# Drinks Ordered per Customer   Mean              Drinks - Mean           (Drinks - Mean)^2     Relative Frequency    Rel Freq * (Drinks - Mean)^2
+# 0                             1.68                  -1.68                      2.82                 0.025                    0.07
+# 1                             1.68                  -0.68                      0.46                 0.550                    0.25  
+# 2                             1.68                   0.32                      0.10                 0.250                    0.03
+# 3                             1.68                   1.32                      1.74                 0.100                    0.17
+# 4                             1.68                   2.32                      5.38                 0.050                    0.27
+# 5                             1.68                   3.32                      11.02                0.025                    0.28
+                                                                                                                  # Variance = 1.07  
+                                                                                                        # Standard Deviation = 1.03     
+                                                                                                        #   (Variance Square Root)
+
+##### Discrete Range                            (Mean + Number of SD From Mean * Variance)
+# Number of Standard Deviation From the Mean                    Number of                           Discrete Range
+# 3                                                              4.77                                   4
+# 2                                                              3.74                                   3
+# 1                                                              2.71                                   2
+# 0                                                              1.68
+# -1                                                             0.65                                   1
+# -2                                                            -0.38                                   0
+# -3                                                            -1.41                                   0
