52906c2d4a8d3dde433ef0a74304a00946bacc75
Brennan Grout <groutb@orvis.com>
2024-03-19 11:00:54 -0400

    added discrete vs continuous file

---
+++ discrete_vs_continuous.R
@@ -0,0 +1,11 @@
+# Discrete Mean
+
+#################                            Relative Frequencey          Weight
+# Drinks Ordered per Customer   Frequency   (Frequency / Total Drinks)  (Freq * Rel Freq)  
+# 0                                1                0.025                   0.000
+# 1                               22                0.550                   0.550
+# 2                               10                0.250                   0.500
+# 3                               4                 0.100                   0.300
+# 4                               2                 0.050                   0.200
+# 5                               1                 0.025                   0.125
+# Total Customers               # 40              # MEAN                  # 1.68
